77c2e262c2482a40e76e774da120508020d425eb
Mahavatar Daniel <mahavatar5d@gmail.com>
2022-12-13 17:08:02 -0300

    Init commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        node: true,
+    },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    root: true,
+    rules: {
+        // override/add rules settings here, such as:
+        // 'vue/no-unused-vars': 'error'
+    },
+};
