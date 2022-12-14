90955fd205353453e5666765fe186d2ded79b2ec
Mahavatar5D <mahavatar5d@gmail.com>
2022-12-13 21:54:47 -0300

    Change .eslintrc.cjs to include .tsconfig.js parser

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,17 @@
 module.exports = {
     env: {
-        node: true,
+        node: true
     },
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
     parser: "@typescript-eslint/parser",
+    parserOptions: { project: ["./tsconfig.json"] },
     plugins: ["@typescript-eslint"],
     root: true,
     rules: {
         // override/add rules settings here, such as:
         // 'vue/no-unused-vars': 'error'
-    },
+    }
 };
